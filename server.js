767ee4bddb8ace2c4b167b4f2033e03000532f16
katukenda <janithaprashadkatukenda@gmail.com>
2025-11-14 23:08:21 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Backend API is running...');
+});
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.listen(5000, () => console.log('Server running on port 5000'));
